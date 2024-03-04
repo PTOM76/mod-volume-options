c43caf980ff000687bfc0c55ccd36d308b7c77a5
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-03-05 01:56:22 +0900

    gradle

--- common/src/main/java/net/pitan76/mvo76/screen/NotMCPitanLibScreen.java
+++ common/src/main/java/net/pitan76/mvo76/screen/NotMCPitanLibScreen.java
@@ -1,16 +1,16 @@
 package net.pitan76.mvo76.screen;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ScreenTexts;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.LiteralText;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.Text;
 import net.pitan76.mcpitanlib.api.util.client.ScreenUtil;
 
 public class NotMCPitanLibScreen extends Screen {
     protected Screen parent;
 
     public NotMCPitanLibScreen(Screen parent) {
-        super(new LiteralText(""));
+        super(Text.literal(""));
         this.parent = parent;
     }
 
@@ -28,10 +28,10 @@ public class NotMCPitanLibScreen extends Screen {
     }
 
     @Override
-    public void render(MatrixStack stack, int mouseX, int mouseY, float delta) {
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         try {
             String str = "Not MCPitanLib. Required for this screen.";
-            drawCenteredTextWithShadow(stack, textRenderer, new LiteralText(str).asOrderedText(), width / 2, height / 2, 0xFFFFFF);
+            context.drawCenteredTextWithShadow(textRenderer, Text.literal(str).asOrderedText(), width / 2, height / 2, 0xFFFFFF);
         } catch (Exception e) {
             e.printStackTrace();
         }
